a5530725addd53b1f74e2b69df0c8a73a2fd2265
Chenyuhan_Shen <chenyuhan.shen@my.jcu.edu.au>
2022-01-11 01:24:07 +0800

    Finish unreliable car test.

---
+++ unreliable_car_test.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 Practical
+Unreliable Car Test
+By Chenyuhan Shen
+11/01/2021
+"""
+
+from unreliable_car import UnreliableCar
+
+#1.
+old_car = UnreliableCar("Old Car", 100, 70)
+
+#2.
+old_car.drive(40)
+
+#3.
+print(old_car)
+
+#4.
+old_car.drive(100)
+
+#5.
+print(old_car)
